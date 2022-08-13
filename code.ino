5831afc8797fc0cabe90d8d5eaf1ad6a6c5c97eb
ashish malla <74368820+im45145v@users.noreply.github.com>
2022-08-13 17:47:06 +0530

    Show Starts Now

---
+++ code.ino
@@ -0,0 +1,10 @@
+//libs
+#include <ESP8266Firebase.h>
+#include <ESP8266WiFi.h>
+#include <Keypad.h>
+#include <LiquidCrystal_I2C.h>
+//vars
+#define FINGERPRINT ""
+#define _SSID ""   
+#define _PASSWORD ""    
+#define PROJECT_ID "otp-generator-12592" 
